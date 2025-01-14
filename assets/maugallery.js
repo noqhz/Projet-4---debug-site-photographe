dfa60abf3980ceb70bf64e4842abd87bb3a361ac
Julien Fritsch <jfritsch.pro@gmail.com>
2025-01-14 18:09:45 +0100

    espacements index +1 -1 , rajout fichiers bootstrap, meta description, contraste nav-link gallery, Hn section services

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -150,7 +150,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i-1;
+          index = i - 1;
         }
       });
       next =
@@ -189,7 +189,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i+1;
+          index = i + 1;
         }
       });
       next = imagesCollection[index] || imagesCollection[0];
